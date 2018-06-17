ed704cd6fe6a94060a0bd4134c461587f54321c6
unknown <aashusmaileforever@gmail.com>
2018-06-17 17:08:15 +0530

    chrome developer consoles option

---
+++ main.js
@@ -0,0 +1,51 @@
+var mobileData = [{
+        mobileName: "Apple",
+        prize: "50000"
+    },
+    {
+        mobileName: "Mi Note5",
+        prize: "12000"
+    },
+    {
+        mobileName: "Sony",
+        prize: "10,000"
+    }
+];
+
+
+
+// regular
+console.log("Hi zuber keep doing");
+// interpolated
+console.log("hi zuber", "+ppp");
+// styled
+console.log("%c Hi i am logest text", 'font-size:30px; color:red');
+// warning
+console.warn("Ho my god i leart a javascript");
+// error
+console.error("OH No!!! you lost");
+//info
+console.info("zuber will learn the fronted work as soon as");
+// testing
+console.assert(1 === 2, "oh no this is wrong");
+// celaring
+//console.clear();
+// viwing dom elements
+// console.log(p);
+// console.dir(p);
+
+// grouping together
+console.count("zuber");
+console.count("zuber");
+console.count("zuber");
+console.count("zuber");
+console.count("zuber");
+console.count("zuber");
+console.count("khan");
+console.count("khan");
+
+// timimg
+console.time("sefer api timiging");
+console.timeEnd("this is bad time");
+// table
+console.table(mobileData);
